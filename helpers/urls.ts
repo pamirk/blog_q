6d7d1ad62eb39eebf6280c61ea7499a4c554c7cc
pamir <pkcomsats@gmail.com>
2022-07-12 11:35:32 +0500

    fix: fixing 404 on refresh v4

--- helpers/urls.ts
+++ helpers/urls.ts
@@ -4,24 +4,24 @@ import { redactEmailAddresses } from 'helpers/text';
  * Get query params from window.location.search
  */
 export function getQueryParams( redact = false ): { [k: string]: string } {
-    try {
-        return window.location.search.substring( 1 ).split( '&' ).filter( Boolean ).reduce( ( acc, param ) => {
-            let [ key, val ] = param.split( '=' );
-            key = decodeURIComponent( key );
-            val = decodeURIComponent( val );
+	try {
+		return window.location.search.substring( 1 ).split( '&' ).filter( Boolean ).reduce( ( acc, param ) => {
+			let [ key, val ] = param.split( '=' );
+			key = decodeURIComponent( key );
+			val = decodeURIComponent( val );
 
-            if ( redact ) {
-                val = redactEmailAddresses( val );
-            }
+			if ( redact ) {
+				val = redactEmailAddresses( val );
+			}
 
-            return {
-                ...acc,
-                [ key ]: val,
-            };
-        }, {} );
-    } catch ( err ) {
-        return {};
-    }
+			return {
+				...acc,
+				[ key ]: val,
+			};
+		}, {} );
+	} catch ( err ) {
+		return {};
+	}
 }
 
 /**
@@ -29,55 +29,56 @@ export function getQueryParams( redact = false ): { [k: string]: string } {
  * returns either a false (external URL) or a relative path.
  */
 export function getRelativeLink( link: string ) {
-    if ( !link ) {
-        return false;
-    }
-    // These tests validate whether a permalink is governed by this app.
-    const appDomainTests = [
-        /^(qz\.com|quartzy\.qz\.com|work\.qz\.com)$/, // allowed subdomains of qz.*
-        /(go-vip\.co|quartz\.work|qz\.dev|qz\.vip)$/, // dev domains
-    ];
+	if ( !link ) {
+		return false;
+	}
+	// These tests validate whether a permalink is governed by this app.
+	const appDomainTests = [
+		/^(qz\.com|quartzy\.qz\.com|work\.qz\.com)$/, // allowed subdomains of qz.*
+		/(go-vip\.co|quartz\.work|qz\.dev|qz\.vip)$/, // dev domains
+	];
 
-    const urlIsAbsolute = link.match( /^[A-z]\w+\:/ );
-    const httpUrlMatches = link.match( /^https?\:\/\/(([^:\/?#]*))(?:\:([0-9]+))?/ );
+	const urlIsAbsolute = link.match( /^[A-z]\w+\:/ );
+	const httpUrlMatches = link.match( /^https?\:\/\/(([^:\/?#]*))(?:\:([0-9]+))?/ );
 
-    // Some links are special and should not be served by React (a.k.a., force a
-    // round-trip to the server).
-    const staticPaths = [
-        /^\/(app|tips)\/?$/,
-        /^https?:\/\/business-news.live\/?$/,
-        /^https?:\/\/qz.com\/(app|tips)\/?$/,
-        /^\/sitemap\/?/,
-        /^https?:\/\/qz.com\/sitemap\/?/,
-    ];
-    if ( staticPaths.some( regex => regex.test( link ) ) ) {
-        return false;
-    }
+	// Some links are special and should not be served by React (a.k.a., force a
+	// round-trip to the server).
+	const staticPaths = [
+		/^\/(app|tips)\/?$/,
+		/^https?:\/\/business-news.live\/?$/,
+		/^https?:\/\/qz.com\/(app|tips)\/?$/,
+		/^\/sitemap\/?/,
+		/^https?:\/\/qz.com\/sitemap\/?/,
+	];
+	if ( staticPaths.some( regex => regex.test( link ) ) ) {
+		return false;
+	}
 
-    if ( httpUrlMatches ) {
-        const [ site, domain ] = httpUrlMatches;
+	if ( httpUrlMatches ) {
+		const [ site, domain ] = httpUrlMatches;
 
-        if ( appDomainTests.some( regex => regex.test( domain ) ) ) {
-            // URL is absolute and links to one of our domains
-            return link
-                // Make the URL relative by replacing the root part of the URL with a slash
-                .replace( site, '/' )
+		if ( appDomainTests.some( regex => regex.test( domain ) ) ) {
+			// URL is absolute and links to one of our domains
+			return link
+				// Make the URL relative by replacing the root part of the URL with a slash
+				.replace( site, '/' )
+                .replace(domain, 'business-news.live/')
                 // Remove the /app URL part if it exists at the end of an article URL. These URLs are just for the Quartz app.
-                .replace( /\/([0-9]+)\/([^\/]+)\/app\/?$/, ( match, articleId, articleSlug ) => `/${articleId}/${articleSlug}/` )
-                // Replace any instances of multiple slashes
-                .replace( /\/+/g, '/' );
-        }
+				.replace( /\/([0-9]+)\/([^\/]+)\/app\/?$/, ( match, articleId, articleSlug ) => `/${articleId}/${articleSlug}/` )
+				// Replace any instances of multiple slashes
+				.replace( /\/+/g, '/' );
+		}
 
-        // URL points to another domain
-        return false;
-    }
+		// URL points to another domain
+		return false;
+	}
 
-    // URL is absolute but not associated with this app
-    if ( urlIsAbsolute ) {
-        return false;
-    }
+	// URL is absolute but not associated with this app
+	if ( urlIsAbsolute ) {
+		return false;
+	}
 
-    return link;
+	return link;
 }
 
 /**
@@ -85,63 +86,63 @@ export function getRelativeLink( link: string ) {
  * just the shortlink in format {domain}/{[postId]}
  */
 export function getShortUrl( url: string ) {
-    const queryParams = url.split( '?' );
-    let shortUrl = url.split( /(\/[\d]+\/)/ ).slice( 0, 2 ).join( '' );
-    // keep utm params if they exist
-    if ( queryParams[1] ) {
-        shortUrl = `${shortUrl}?${queryParams[1]}`;
-    }
-    return shortUrl;
+	const queryParams = url.split( '?' );
+	let shortUrl = url.split( /(\/[\d]+\/)/ ).slice( 0, 2 ).join( '' );
+	// keep utm params if they exist
+	if ( queryParams[1] ) {
+		shortUrl = `${shortUrl}?${queryParams[1]}`;
+	}
+	return shortUrl;
 }
 
 // lifted from https://davidwalsh.name/query-string-javascript
 export function getUrlParameter( name: string ) {
-    const queryParam = name.replace( /[\[]/, '\\[' ).replace( /[\]]/, '\\]' );
-    const regex = new RegExp( `[\\?&]${queryParam}=([^&#]*)` );
-    const results = typeof window !== 'undefined' ? regex.exec( window.location.search ) : null;
+	const queryParam = name.replace( /[\[]/, '\\[' ).replace( /[\]]/, '\\]' );
+	const regex = new RegExp( `[\\?&]${queryParam}=([^&#]*)` );
+	const results = typeof window !== 'undefined' ? regex.exec( window.location.search ) : null;
 
-    return results === null ? '' : decodeURIComponent( results[1].replace( /\+/g, ' ' ) );
+	return results === null ? '' : decodeURIComponent( results[1].replace( /\+/g, ' ' ) );
 }
 
 export function stringifyUrlParams( dict: { [k: string]: any } ) {
-    const keys = Object.keys( dict );
+	const keys = Object.keys( dict );
 
-    if ( keys.length === 0 ) {
-        return '';
-    }
+	if ( keys.length === 0 ) {
+		return '';
+	}
 
-    const queryString = keys.map( key => `${key}=${dict[key]}` ).join( '&' );
+	const queryString = keys.map( key => `${key}=${dict[key]}` ).join( '&' );
 
-    return `?${queryString}`;
+	return `?${queryString}`;
 }
 
 export function changeToHttps( url: string ) {
-    const regex = new RegExp( '^http\:\/\/' );
-    return url.replace( regex, 'https://' );
+	const regex = new RegExp( '^http\:\/\/' );
+	return url.replace( regex, 'https://' );
 }
 
 /**
  * Extracts the hostname from a URL (no protocol, port, or slashes).
  */
 export function getHostname( url: string ) {
-    try {
-        const { hostname } = new URL( url );
-        return hostname;
-    } catch ( err ) {}
+	try {
+		const { hostname } = new URL( url );
+		return hostname;
+	} catch ( err ) {}
 
-    return '';
+	return '';
 }
 
 /**
  * Extracts the path from a URL with initial slash.
  */
 export function getPath( url: string ) {
-    try {
-        const { pathname } = new URL( url );
-        return pathname;
-    } catch ( err ) {}
+	try {
+		const { pathname } = new URL( url );
+		return pathname;
+	} catch ( err ) {}
 
-    return '';
+	return '';
 }
 
 /**
@@ -166,22 +167,22 @@ export const isAppArticle = ( path: string ) => /\/.+\/app\/?$/.test( path );
  * @return {String|Object} the file extenion or null
  */
 export function getFileExtension( url: string ) {
-    const matches = url.match( /\.([A-z0-9]+)(?:[^\.]+)?$/ );
+	const matches = url.match( /\.([A-z0-9]+)(?:[^\.]+)?$/ );
 
-    if ( matches ) {
-        const [ , extension ] = matches;
-        return extension;
-    }
+	if ( matches ) {
+		const [ , extension ] = matches;
+		return extension;
+	}
 
-    return null;
+	return null;
 }
 
 /**
  * @param {String} url Ensure that a url has a trailing slash
  */
 export function ensureTrailingSlash( url: string ) {
-    if ( url.slice( -1 ) === '/' ) {
-        return url;
-    }
-    return `${url}/`;
+	if ( url.slice( -1 ) === '/' ) {
+		return url;
+	}
+	return `${url}/`;
 }
